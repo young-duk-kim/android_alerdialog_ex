593ef1b0767238814f5b8d24dbd4131598058725
kimyd34 <tommykimyd98@gmail.com>
2021-06-18 19:07:01 +0900

    progress_bar 예제

--- app/src/main/java/com/example/android_inflater_ex/MainActivity.java
+++ app/src/main/java/com/example/android_inflater_ex/MainActivity.java
@@ -3,10 +3,12 @@ package com.example.android_inflater_ex;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,43 +18,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button = findViewById(R.id.layout_popup);
+        ProgressBar progressBar = findViewById(R.id.progress_Bar);
+        progressBar.setIndeterminate(false);
+
+        Button button = findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                showMessage();
+                ProgressDialog dialog = new ProgressDialog(MainActivity.this);
+                dialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+                dialog.setMessage("확인중");
+
+                dialog.show();
             }
         });
     }
 
     private void showMessage() {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("안내");
-        builder.setMessage("종료하시겠습니까");
-        builder.setIcon(R.drawable.ic_launcher_foreground);
-
-        builder.setPositiveButton("예", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                Toast.makeText(getApplicationContext(),"예 버튼을 눌렀습니다.",Toast.LENGTH_LONG).show();
-            }
-        });
-
-        builder.setNeutralButton("취소", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                Toast.makeText(getApplicationContext(),"종료 버튼을 눌렀습니다.",Toast.LENGTH_LONG).show();
-            }
-        });
-
-        builder.setNegativeButton("아니오", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                Toast.makeText(getApplicationContext(),"아니오 버튼을 눌렀습니다.",Toast.LENGTH_LONG).show();
-            }
-        });
-
-        builder.create().show();
-
     }
 }
